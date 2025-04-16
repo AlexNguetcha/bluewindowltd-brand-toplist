af41c1a11ca1af05685b6c6394ca88f1bdaf1101
Alex Nguetcha <nguetchaalex@gmail.com>
2025-04-16 21:08:53 +0100

    style: Add basic containers style

--- resources/views/toplist.blade.php
+++ resources/views/toplist.blade.php
@@ -1,13 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Brand top list</title>
     <link rel="stylesheet" href="{{ asset('css/toplist.css') }}">
+    <style>
+        body {
+            padding: 0 !important;
+            margin: 0 !important;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+
+        .container {
+            width: 100%;
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .toplist-container {
+            border: 1px solid #ccc;
+            padding: 10px;
+        }
+
+        .toplist-container_title {
+            margin: 0;
+        }
+
+        .toplist-container_title h2 {
+            padding: 12px;
+            margin-bottom: 0;
+            color: #fff;
+            text-shadow: 0 1px 1px rgba(0, 0, 0, .5);
+            text-align: left;
+            border-radius: 2px 2px 0 0;
+            background-clip: padding-box;
+            background-image: -webkit-linear-gradient(bottom, #212933 0, #283442 100%);
+            position: relative;
+            font-size: 19px;
+            line-height: 26px;
+            font-weight: 900;
+        }
+    </style>
 </head>
+
 <body>
-    
+    <div class="container">
+        <div class="toplist-container">
+            <div class="toplist-container_title">
+                <h2>Notre sélection des meilleurs casinos en ligne fiables et certifiés par les experts de
+                    CasinoOnlineFrancais en 2025
+                </h2>
+            </div>
+            <div class="toplist-container_brands">
+
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
