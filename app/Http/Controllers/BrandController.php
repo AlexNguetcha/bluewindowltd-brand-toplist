2697e6cc8e96c721df7aa995c2e26b2020120e46
Alex Nguetcha <nguetchaalex@gmail.com>
2025-04-16 08:10:50 +0100

    feat: Add API route for creating and retrieving brands

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BrandController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // Validate the request
+        $validator = Validator::make($request->all(), [
+            'brand_name' => 'required|unique:brands,brand_name',
+            'brand_image' => 'required|url',
+            'rating' => 'required|integer|between:1,5',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Create a new brand
+        $brand = Brand::create([
+            'brand_name' => $request->brand_name,
+            'brand_image' => $request->brand_image,
+            'rating' => $request->rating,
+        ]);
+
+        return response()->json($brand, 201);
+    }
+
+    public function index()
+    {
+        $brands = Brand::all();
+        return response()->json($brands, 200);
+    }
 }
